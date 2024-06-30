61e1ba0015a0e8d2b267c27ee6293a7ba5bbb5cc
Philipp Schaffrath <philipp.schaffrath@gmail.com>
2024-06-30 02:24:36 +0200

    run pinentry with stdin/stdout

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,7 @@ use std::io::{stdin, stdout};
 use pinentry::Pinentry;
 
 fn main() {
-    let mut pinentry = Pinentry::new();
+    let mut pinentry = Pinentry::new(stdin().lock(), stdout());
+
+    pinentry.run();
 }
